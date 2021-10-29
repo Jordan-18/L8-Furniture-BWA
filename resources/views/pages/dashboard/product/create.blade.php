a4edc77c2fd7adc191b91059510f9d49a3d1bf61
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2021-10-29 23:28:14 +0700

    1/2

---
+++ resources/views/pages/dashboard/product/create.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Product &raquo; Create 
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                @if ($error->any())
+                    <div class="mb-5" role="alert">
+                        <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                            There's something wrong!
+                        </div>
+                        <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                            <p>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li> {{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                @endif
+                <form action=""></form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

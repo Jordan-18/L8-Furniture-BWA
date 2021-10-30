3300e0eded79aef0ca055803b8d148cf7b9d4dab
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2021-10-30 15:28:51 +0700

    penambahan create product

--- resources/views/pages/dashboard/product/create.blade.php
+++ resources/views/pages/dashboard/product/create.blade.php
@@ -7,7 +7,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div>
-                @if ($error->any())
+                @if ($errors->any())
                     <div class="mb-5" role="alert">
                         <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
                             There's something wrong!
@@ -23,8 +23,45 @@
                         </div>
                     </div>
                 @endif
-                <form action=""></form>
+                <form action="{{ route('dashboard.product.store') }}" class="w-full" method="post" enctype="multipart/form-data">
+                    @csrf
+                    {{-- Name --}}
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Name</label>
+                            <input type="text" value="{{ old('name')}}" name="name" placeholder="Product Name" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500" >
+                        </div>
+                    </div>
+                    {{-- Desk --}}
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Description</label>
+                            <textarea name="deskripsi" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500" >
+                                {!! old('diskripsi') !!}
+                            </textarea>
+                        </div>
+                    </div>
+                    {{-- Price --}}
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Price</label>
+                            <input type="number" value="{{ old('price')}}" name="price" placeholder="price" class="block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500" >
+                        </div>
+                    </div>
+                    {{-- Button --}}
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <button type="submit" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded shadow-lg">
+                                Save Product
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+    <script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'deskripsi' );
+    </script>
 </x-app-layout>

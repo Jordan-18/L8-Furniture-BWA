8f948ac946fbff49c546fa788b796b10bd2aca48
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2021-11-20 11:47:40 +0700

    penambahan randomview

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.frontend.index');
+        $products = Product::with(['galleries'])->latest()->get();
+        return view('pages.frontend.index', compact('products'));
     }
     public function details(Request $request, $slug)
     {
-        return view('pages.frontend.details');
+        $product = Product::with(['galleries'])->where('slug', $slug)->firstOrFail();
+        $recomendations = Product::with(['galleries'])->inRandomOrder()->limit(4)->get();
+
+        return view('pages.frontend.details', compact('product', 'recomendations'));
     }
     public function cart(Request $request)
     {
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,7 @@ class Product extends Model
     ];
 
     public function galleries(){
-        return $this->hasMany(ProductGallery::class,'products_id','id');
+        return $this->hasMany(ProdukGallery::class,'product_id','id');
     }
 
 }
